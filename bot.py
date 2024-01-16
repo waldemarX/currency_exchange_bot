d876be1fa0d419f11a18b722cb22cb5b9c0b17d0
Vladimir Ermakov <145784290+waldemarX@users.noreply.github.com>
2024-01-16 16:15:05 +0300

    add lexicon, handlers

--- bot.py
+++ bot.py
@@ -1,7 +1,32 @@
-from config_data.config import load_config
+import asyncio
+from aiogram import Bot, Dispatcher
+from aiogram.types import Message
+from aiogram.filters import Command, CommandStart
+from config_data.config import Config, load_config
 
 
-config = load_config('.env')
+# Этот хэндлер срабатывает на команду /start
+@dp.message(CommandStart())
+async def process_start_command(message: Message):
+    await message.answer(text=LEXICON_RU['/start'])
 
-bot_token = config.tg_bot.token
-superadmin = config.tg_bot.admin_ids[0]
+
+# Этот хэндлер срабатывает на команду /help
+@dp.message(Command(commands='help'))
+async def process_help_command(message: Message):
+    await message.answer(text=LEXICON_RU['/help'])
+
+
+async def main() -> None:
+
+    config: Config = load_config()
+
+    bot = Bot(token=config.tg_bot.token)
+    dp = Dispatcher()
+
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
