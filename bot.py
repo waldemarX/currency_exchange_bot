ee4092833ea2783ca3fcfe983d9fd44cb96c9108
Vladimir Ermakov <145784290+waldemarX@users.noreply.github.com>
2024-01-16 16:18:13 +0300

    update u_h to 2 commands

--- bot.py
+++ bot.py
@@ -1,20 +1,6 @@
 import asyncio
 from aiogram import Bot, Dispatcher
-from aiogram.types import Message
-from aiogram.filters import Command, CommandStart
 from config_data.config import Config, load_config
-
-
-# Этот хэндлер срабатывает на команду /start
-@dp.message(CommandStart())
-async def process_start_command(message: Message):
-    await message.answer(text=LEXICON_RU['/start'])
-
-
-# Этот хэндлер срабатывает на команду /help
-@dp.message(Command(commands='help'))
-async def process_help_command(message: Message):
-    await message.answer(text=LEXICON_RU['/help'])
 
 
 async def main() -> None:
